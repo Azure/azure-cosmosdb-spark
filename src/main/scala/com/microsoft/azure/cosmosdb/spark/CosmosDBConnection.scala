f633479feda59037a12e80ce7278276057964cb3
Ramnandan Krishnamurthy <nandparikrish@gmail.com>
2018-06-22 16:22:11 -0700

    Increase BulkExecutor initialization retry policy

--- src/main/scala/com/microsoft/azure/cosmosdb/spark/CosmosDBConnection.scala
+++ src/main/scala/com/microsoft/azure/cosmosdb/spark/CosmosDBConnection.scala
@@ -92,6 +92,10 @@ private[spark] case class CosmosDBConnection(config: Config) extends LoggingTrai
 
   def getDocumentBulkImporter(collectionThroughput: Int, partitionKeyDefinition: Option[String]): DocumentBulkExecutor = {
     if (bulkImporter == null) {
+      val initializationRetryOptions = new RetryOptions()
+      initializationRetryOptions.setMaxRetryAttemptsOnThrottledRequests(1000)
+      initializationRetryOptions.setMaxRetryWaitTimeInSeconds(1000)
+
       if (partitionKeyDefinition.isDefined) {
         val pkDefinition = new PartitionKeyDefinition()
         val paths: ListBuffer[String] = new ListBuffer[String]()
@@ -103,7 +107,7 @@ private[spark] case class CosmosDBConnection(config: Config) extends LoggingTrai
           collectionName,
           pkDefinition,
           collectionThroughput
-        ).build()
+        ).withInitializationRetryOptions(initializationRetryOptions).build()
       }
       else {
         bulkImporter = DocumentBulkExecutor.builder.from(documentClient,
@@ -111,7 +115,7 @@ private[spark] case class CosmosDBConnection(config: Config) extends LoggingTrai
           collectionName,
           getCollection.getPartitionKey,
           collectionThroughput
-        ).build()
+        ).withInitializationRetryOptions(initializationRetryOptions).build()
       }
     }
 
@@ -165,6 +169,7 @@ private[spark] case class CosmosDBConnection(config: Config) extends LoggingTrai
       connectionPolicy.setPreferredLocations(preferredLocations)
     }
 
+    /*
     val bulkimport = config.get[String](CosmosDBConfig.BulkImport).
       getOrElse(CosmosDBConfig.DefaultBulkImport.toString).
       toBoolean
@@ -175,6 +180,7 @@ private[spark] case class CosmosDBConnection(config: Config) extends LoggingTrai
       connectionPolicy.getRetryOptions.setMaxRetryAttemptsOnThrottledRequests(0)
       connectionPolicy.setConnectionMode(ConnectionMode.Gateway)
     }
+    */
 
     ClientConfiguration(
       config.get[String](CosmosDBConfig.Endpoint).get,
