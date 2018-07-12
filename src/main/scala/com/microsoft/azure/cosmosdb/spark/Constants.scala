1ac2e6ee9094b4506fe2b1e6196249a3497612b9
Ramnandan Krishnamurthy <nandparikrish@gmail.com>
2018-07-11 19:14:03 -0700

    Bump Spark connector version to 1.2.0 + Version change of sync CosmosDB Java SDK to 1.16.1

--- src/main/scala/com/microsoft/azure/cosmosdb/spark/Constants.scala
+++ src/main/scala/com/microsoft/azure/cosmosdb/spark/Constants.scala
@@ -23,6 +23,6 @@
 package com.microsoft.azure.cosmosdb.spark
 
 object Constants {
-  val currentVersion = "2.3.0_2.11-1.1.1"
+  val currentVersion = "2.3.0_2.11-1.2.0"
   val userAgentSuffix = s" SparkConnector/$currentVersion"
 }
